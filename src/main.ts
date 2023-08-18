772c6c837d4b3d7a83f7f5330ec2ef58d6d06094
Maxence <noizetmaxencepro@gmail.com>
2023-08-18 19:10:01 +0200

    feat(helmet): add helmet and re-organize configurations

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,14 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from "helmet";
 import pJson from "../package.json";
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle("Documentation for nest-api")
-    .setDescription("This is the documentation for the nest-api, NestJS api with various features.")
-    .setVersion(pJson.version)
-    .addBearerAuth()
-    .build();
-
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup("/", app, swaggerDocument, {
-    swaggerOptions: {
-      persistAuthorization: true
-    }
-  });
+  app.use(helmet());
 
   app.enableCors({
     credentials: true,
@@ -27,6 +16,24 @@ async function bootstrap(): Promise<void> {
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle("Documentation for nest-api")
+      .setDescription(
+        "This is the documentation for the nest-api, NestJS api with various features."
+      )
+      .setVersion(pJson.version)
+      .addBearerAuth()
+      .build()
+  );
+
+  SwaggerModule.setup("/", app, swaggerDocument, {
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  });
 
   await app.listen(+process.env.API_PORT);
 }
