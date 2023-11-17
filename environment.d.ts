ac0f1084232408a479b83a9a24d1eb95f98701c4
Maxence <noizetmaxencepro@gmail.com>
2023-11-17 19:23:43 +0100

    fix(joi): update joi verifications and add pino_pretty

--- environment.d.ts
+++ environment.d.ts
@@ -1,13 +1,14 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      API_PORT: string;
+      API_PORT: number;
       DATABASE_HOST: string;
-      DATABASE_PORT: string;
+      DATABASE_PORT: number;
       DATABASE_USERNAME: string;
       DATABASE_PASSWORD: string;
       DATABASE_NAME: string;
       TOKEN_EXPIRES_IN: string;
+      PINO_PRETTY: boolean;
     }
   }
 }
