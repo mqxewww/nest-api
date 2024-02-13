3056f9f0867b8d7411b4aee17f6edcc373cb5ea7
Maxence NOIZET <noizetmaxence@proton.me>
2024-02-13 22:42:25 +0100

    fix(prod): fix start:prod command and mikro-orm config

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   dbName: process.env.DATABASE_NAME,
-  entities: ["./dist/**/entities/*.entity.js"],
+  entities: ["./src/**/entities/*.entity.js"],
   entitiesTs: ["./src/**/entities/*.entity.ts"],
   extensions: [SeedManager],
   allowGlobalContext: false,
